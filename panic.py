17fa6427e1adc02eff9a735aaa6ab72c73fcd483
redpois0n <red@cmail.nu>
2015-03-26 13:43:16 +0100

    Function for getting all addresses, broadcast function

--- panic.py
+++ panic.py
@@ -7,6 +7,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 argparse = argparse.ArgumentParser()
 argparse.add_argument("-b", "--bport", dest = "buttonport", help = "Port where HTTP server will listen")
 argparse.add_argument("-s", "--sport", dest = "signalport", help = "Port where UDP server will listen and broadcast signal to")
+argparse.add_argument("-k", "--key", dest = "key", help = "Signal password, preventing unauthorized machines to trigger panic")
 args = argparse.parse_args()
 
 global button_port
@@ -14,6 +15,18 @@ button_port = int(args.buttonport or 8080)
 
 global signal_port
 signal_port = int(args.signalport or 1301)
+
+global key
+key = args.key
+
+if key is None:
+    raise Exception("No key specified")
+
+
+def getBroadcastAddresses():
+    addresses = []
+    
+    return addresses
 
 def createSocket():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -28,9 +41,17 @@ def broadcastListener():
     while True:
         try:
             message, address = s.recvfrom(1024)
-            print("Decoded: " + message.decode())
+            if message.decode() == key:
+                broadcast()
         except:
             pass
+
+def broadcast():
+    addresses = getBroadcastAddresses()
+    for address in addresses:
+        s = createSocket()
+        s.sendto(key.encode(), (address, signal_port))
+        s.close()
 
 class HTTPHandler(BaseHTTPRequestHandler):
     def do_GET(self):
