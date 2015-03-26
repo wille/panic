a73684056b3f6f59f737ef0a4ee7a296ddcebc13
redpois0n <red@cmail.nu>
2015-03-26 12:37:00 +0100

    HTTP and broadcast listener

--- panic.py
+++ panic.py
@@ -1,4 +1,8 @@
 import argparse
+import threading
+import platform
+import socket
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 argparse = argparse.ArgumentParser()
 argparse.add_argument("-b", "--bport", dest = "buttonport")
@@ -11,5 +15,33 @@ button_port = int(args.buttonport or 8080)
 global signal_port
 signal_port = int(args.signalport or 1301)
 
+def createSocket():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    return s
+
+def broadcastListener():
+    s = createSocket()
+    s.bind(("", signal_port))
+
+    while True:
+        try:
+            message, address = s.recvfrom(1024)
+            print("Decoded: " + message.decode())
+        except:
+            pass
+
+class HTTPHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("Got request: " + self.path);
+
+def createServer():
+    server = HTTPServer(("", button_port), HTTPHandler)
+    server.serve_forever()
+
 if __name__ == "__main__":
-    pass
+    print("Detected system: " + platform.system())
+
+    threading.Thread(name="UDP thread", target=broadcastListener).start()
+    threading.Thread(name="HTTP thread", target=createServer).start()
